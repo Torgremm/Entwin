11eaee9d4c1dd528955f41455dffe501a7e61971
torgremm-admin <tormodnes@gmail.com>
2025-07-25 15:12:33 +0200

    init commit

---
+++ Entwin.API/Program.cs
@@ -0,0 +1,19 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddScoped(sp =>
+    new HttpClient { BaseAddress = new Uri("http://localhost:5265/") });
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.MapControllers();
+app.Run();
