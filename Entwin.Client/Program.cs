1dc08f923e6eb787f8a03481153fed254468c55a
torgremm-admin <tormodnes@gmail.com>
2025-07-29 00:30:36 +0200

    is fine

--- Entwin.Client/Program.cs
+++ Entwin.Client/Program.cs
@@ -7,12 +7,12 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+
 var apiBaseUrl = builder.Configuration["ApiBaseUrl"];
 if (string.IsNullOrWhiteSpace(apiBaseUrl))
 {
     throw new Exception("API base URL not configured in appsettings.json");
 }
-
 builder.Services.AddScoped(sp =>
     new HttpClient { BaseAddress = new Uri(apiBaseUrl) });
 
